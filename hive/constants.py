bd14640fd6e1bf8ce2301c4f99dbbc57addbf77f
= <43090520+youssefashmawy@users.noreply.github.com>
2024-11-24 22:09:07 +0200

    Changed files
    Added Main directory for GUI logic

--- hive/constants.py
+++ hive/constants.py
@@ -9,3 +9,41 @@ WHITE = (255, 255, 255)
 GREY = (128, 128, 128)
 
 FPS = 60
+
+# Images
+
+# Black pieces
+black_ant = pygame.transform.scale(pygame.image.load("Assets/black_ant.png"), (44, 24))
+black_spider = pygame.transform.scale(
+    pygame.image.load("Assets/black_spider.png"), (44, 24)
+)
+black_queen = pygame.transform.scale(
+    pygame.image.load("Assets/black_queen.png"), (44, 24)
+)
+black_grasshopper = pygame.transform.scale(
+    pygame.image.load("Assets/black_grasshopper.png"), (44, 24)
+)
+black_beetle = pygame.transform.scale(
+    pygame.image.load("Assets/black_beetle.png"), (44, 24)
+)
+
+# White pieces
+white_ant = pygame.transform.scale(pygame.image.load("Assets/white_ant.png"), (44, 24))
+white_spider = pygame.transform.scale(
+    pygame.image.load("Assets/white_spider.png"), (44, 24)
+)
+white_queen = pygame.transform.scale(
+    pygame.image.load("Assets/white_queen.png"), (44, 24)
+)
+white_grasshopper = pygame.transform.scale(
+    pygame.image.load("Assets/white_grasshopper.png"), (44, 24)
+)
+white_beetle = pygame.transform.scale(
+    pygame.image.load("Assets/white_beetle.png"), (44, 24)
+)
+
+
+# Hexagon constants
+
+RADIUS = 30
+HEX_WIDTH = 2 * RADIUS
--- main.py
+++ main.py
@@ -3,3 +3,24 @@ import math
 from hive.constants import WIDTH, HEIGHT, FPS
 
 pygame.init()
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+pygame.display.set_caption("Hive")
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        pygame.display.update()
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
