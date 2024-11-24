4f3ec4c3df309b73f8166535ed6282cff5473056
= <43090520+youssefashmawy@users.noreply.github.com>
2024-11-24 16:45:00 +0200

    Added initial files
    Added the files

---
+++ hive/constants.py
@@ -0,0 +1,11 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 600
+
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+GREY = (128, 128, 128)
+
+FPS = 60
---
+++ main.py
@@ -0,0 +1,5 @@
+import pygame
+import math
+from hive.constants import WIDTH, HEIGHT, FPS
+
+pygame.init()
